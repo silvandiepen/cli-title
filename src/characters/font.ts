c70fda7aab110826db5b1a9ab6f179f07dd210ec
Sil van Diepen <me@sil.mt>
2025-03-13 10:44:46 +0100

    feat: Enhanced font management by introducing FontDefinition and defaultFontConfig

--- src/characters/font.ts
+++ src/characters/font.ts
@@ -5,21 +5,20 @@ export type TwelveLines = [string, string, string, string, string, string,string
 export type ElevenLines = [string, string, string, string, string, string, string, string, string, string, string];
 export type Lines = FiveLines | EightLines | SixLines | TwelveLines | ElevenLines
 
-import { defaultFont } from './fonts/default';
+import { defaultFont, defaultFontConfig } from './fonts/default';
 import { pixelfont } from './fonts/pixels';
-
-import { FontType } from '../types';
+import { FontType, FontDefinition } from '../types';
 import { blockFont } from './fonts/block';
 import { massiveFont } from './fonts/massive';
 import { hybridFont } from './fonts/hybrid';
 import { digitalFont } from './fonts/digital';
 
-export const fonts: { [key in FontType]: { [key: string]: Lines } } = {
-  default: defaultFont,
-  pixel: pixelfont,
-  block: blockFont,
-  massive: massiveFont,
-  hybrid: hybridFont,
-  digital: digitalFont
+export const fonts: { [key in FontType]: FontDefinition } = {
+  default: { characters: defaultFont, config: defaultFontConfig },
+  pixel: { characters: pixelfont },
+  block: { characters: blockFont },
+  massive: { characters: massiveFont },
+  hybrid: { characters: hybridFont },
+  digital: { characters: digitalFont }
 };
 
